e81690ca203c755a7894afdc4ee41f4b9218ed08
Mahmoud Abdullah Anani <99493135+MahmoudAbdullahAnani@users.noreply.github.com>
2023-01-01 13:05:02 +0200

    i18n

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,43 @@
 import "../styles/globals.css";
-import "../i18n";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 // import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
 import { Fragment } from "react";
 import Navbar from "../components/Nabar";
 import Footer from "../components/Footer";
+
+
+
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import lanAr from "../data/locale/lanAr.json";
+import lanEn from "../data/locale/lanEn.json";
+const resources = {
+  en: {
+    translation: lanEn,
+  },
+  ar: {
+    translation: lanAr,
+  },
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: "en",
+
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+
+
+
+
+
+
+
+
+
 function MyApp({ Component, pageProps }) {
   return (
     <Fragment>
